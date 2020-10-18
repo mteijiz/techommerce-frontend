0f995d447d8904e6c8693ccabb3ec56065d39cbe
Matias <matiteijiz@gmail.com>
2020-10-17 21:37:00 -0300

    Adding keycloak url

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const dir = path.join(__dirname+'/dist/techommerce-frontend/index.html')
-app.use(express.static(dir));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/techommerce-frontend/index.html'));});
+app.use(express.static(__dirname + '/dist/techommerce-frontend'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname +
+    '/dist/techommerce-frontend/index.html'));
+});
 app.listen(process.env.PORT || 8080);
